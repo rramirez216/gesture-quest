a07269217a2cd61c0fc1287dcf630646f962cf31
Ruben Ramirez <rramirez110911@gmail.com>
2024-07-31 19:57:04 -0700

    creates new state variable for controlled form input intervalTime

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,16 @@
 import React from 'react'
 import { styled } from '@linaria/react'
+import Home from './pages/Home'
 
 function App() {
-  const [imageList, setImageList] = React.useState([])
-
-  const handleImage = (event) => {
-    let arr = []
-    for (const obj of event.target.files) {
-      let url = URL.createObjectURL(obj)
-      arr = [...arr, url]
-    }
-    setImageList([...arr])
-    console.log(arr)
-  }
-
-  React.useEffect(() => {
-    return () => {
-      if (imageList.length > 0) {
-        URL.revokeObjectURL(image)
-      }
-    }
-  }, [imageList])
-
   return (
     <Main>
-      <FileInput handleImage={handleImage} />
-      {imageList.map((item) => (
-        <Image src={item} alt='' key={crypto.randomUUID()} />
-      ))}
+      <Home />
     </Main>
-  )
-}
-
-function FileInput({ handleImage }) {
-  return (
-    <>
-      <input
-        type='file'
-        name='file'
-        id='file'
-        accept='image/*'
-        onChange={(event) => handleImage(event)}
-        multiple
-      />
-    </>
   )
 }
 
 const Main = styled.main`
   max-height: 100%;
-`
-const Image = styled.img`
-  width: 100px;
-  height: 100px;
 `
 export default App
