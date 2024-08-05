c7ec7ef3cf279566ff2ea1618667db557808517f
Ruben Ramirez <rramirez110911@gmail.com>
2024-08-04 21:15:46 -0700

    basic modal styles added

--- src/components/slider/Modal.jsx
+++ src/components/slider/Modal.jsx
@@ -12,9 +12,15 @@ function Modal() {
 const Wrapper = styled.div`
   display: flex;
   justify-content: center;
-  background-color: hsla(25, 100%, 97% / 25%);
+  background-color: hsl(25deg 100% 97% / 50%);
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
 `
 const ImageWrapper = styled.div`
+  width: 50%;
   height: 100%;
   background-color: hsl(130, 55%, 79%);
 `
