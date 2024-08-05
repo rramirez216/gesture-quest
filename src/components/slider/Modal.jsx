e4ba0b16219cbc00420efd9b5d20840f393dccad
Ruben Ramirez <rramirez110911@gmail.com>
2024-08-04 21:46:42 -0700

    imports new child components

--- src/components/slider/Modal.jsx
+++ src/components/slider/Modal.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { styled } from '@linaria/react'
+import SliderButton from './SliderButton'
 
-function Modal() {
+function Modal({ sliderDisplay, handleSliderDisplay }) {
+  let isSliderOn = sliderDisplay === true ? 'flex' : 'none'
   return (
-    <Wrapper>
+    <Wrapper display={isSliderOn}>
       <ImageWrapper></ImageWrapper>
+      <SliderButton handleSliderDisplay={handleSliderDisplay} />
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
-  display: flex;
+  display: ${(props) => props.isSliderOn};
+  flex-flow: column nowrap;
   justify-content: center;
+  align-items: center;
   background-color: hsl(25deg 100% 97% / 50%);
   position: absolute;
   top: 0;
