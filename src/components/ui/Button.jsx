ca31f84b8c2f38212fd99354a8467cd509d67de5
Ruben Ramirez <rramirez110911@gmail.com>
2024-08-05 20:27:47 -0700

    renames slider button component to button

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { styled } from '@linaria/react'
+
+function Button({ handleSliderDisplay }) {
+  return (
+    <>
+      <Wrapper type='button' onClick={handleSliderDisplay}>
+        start
+      </Wrapper>
+    </>
+  )
+}
+
+const Wrapper = styled.button`
+  font-size: 20px;
+  padding: 4px 16px 4px;
+  &:hover {
+    background-color: hsl(0deg 100% 100%);
+    cursor: pointer;
+  }
+`
+
+export default Button
