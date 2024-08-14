de267c7fb8e48a1d4e02b59c12f3e42864d3b39a
Ruben Ramirez <rramirez110911@gmail.com>
2024-08-13 21:07:44 -0700

    makes changes to the whole file

--- src/components/slider/Timer.jsx
+++ src/components/slider/Timer.jsx
@@ -1,54 +1,53 @@
 import React from 'react'
 import { styled } from '@linaria/react'
 
-function Timer({ intervalTime }) {
-  const [minutes, setMinutes] = React.useState()
-  const [seconds, setSeconds] = React.useState()
+function Timer({ intervalTime, sliderDisplay }) {
+  const [minutes, setMinutes] = React.useState(0)
+  const [seconds, setSeconds] = React.useState(0)
 
   let secondsOrMinutes = intervalTime.slice(-3)
+
   let countdown
 
-  if (secondsOrMinutes == 'min' || secondsOrMinutes == 'mins') {
+  if (secondsOrMinutes == 'min' || secondsOrMinutes == 'ins') {
     countdown = extractNumber(intervalTime) * 60 * 1000
   } else {
     countdown = extractNumber(intervalTime) * 1000
   }
 
-  //if user only inputs seconds create the countdown so that it only multiplies by 1000 milliseconds
-
   React.useEffect(() => {
-    let count = 0
+    if (sliderDisplay) {
+      const intervalID = setInterval(startCountdown, 1000)
+      console.log('its running!')
 
-    const intervalID = setInterval(
-      startCountdown,
-      1000,
-      countdown,
-      stopInterval
-    )
-    console.log(count++)
-
-    function stopInterval() {
-      clearInterval(intervalID)
+      return () => {
+        if (!minutes && seconds < 0) {
+          console.log('all clear!')
+          clearInterval(intervalID)
+        }
+      }
     }
-  }, [seconds])
+  }, [sliderDisplay])
 
-  function startCountdown(duration, stopFunc) {
-    let currentTimer = duration
+  function startCountdown() {
+    let currentTimer = countdown
     let mins = Math.floor((currentTimer % 360000) / 60000)
     let secs = Math.floor((currentTimer % 60000) / 1000)
 
-    console.log(currentTimer)
-
-    currentTimer -= 1000
+    countdown -= 1000
     setMinutes(mins)
     setSeconds(secs)
-
-    if (mins && secs < 0) {
-      stopFunc
-    }
   }
 
-  return <p>{`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`}</p>
+  return (
+    <div>
+      <p>{`${minutes}:${seconds < 10 ? '0' : ''}${seconds}`}</p>
+      {/* <button type='button' onClick={() => handleClick()}>
+
+        click me
+      </button> */}
+    </div>
+  )
 }
 
 function extractNumber(params) {
@@ -58,3 +57,6 @@ function extractNumber(params) {
 }
 
 export default Timer
+
+// TODO: what is a cleanup function? why and when is it needed?
+// TODO: what are effects? why and when to use them?
