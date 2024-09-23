a3487424265deb3964c9c48b50c480722c1abad5
Ruben Ramirez <rramirez110911@gmail.com>
2024-09-22 21:39:10 -0700

    styles fileinput

--- src/components/ui/FileInput.jsx
+++ src/components/ui/FileInput.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { styled } from '@linaria/react'
 
+
 function FileInput({ handleImage }) {
   return (
     <>
-      <input
+      <Input
         type='file'
         name='file'
         id='file'
@@ -15,5 +16,20 @@ function FileInput({ handleImage }) {
     </>
   )
 }
+
+const Input = styled.input`
+&::file-selector-button {
+  padding: 4px 16px 4px;
+  cursor: pointer;
+  background-color: hsl(133deg 47% 88%);
+  border: 2px solid hsl(133deg 50% 82%);
+  color: hsl(195deg 72% 22%);
+  font-size: 1.5rem;
+  &:hover {
+    background-color: hsl(133deg 47% 80%);
+    border: 2px solid hsl(133deg 50% 80%);
+  }
+}
+`
 
 export default FileInput
