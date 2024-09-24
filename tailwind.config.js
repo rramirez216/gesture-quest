a7b47025f54dbff62e94ce832389322f6481fb2b
Ruben Ramirez <rramirez110911@gmail.com>
2024-09-23 20:28:41 -0700

    tailwindcss config files added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      primary: "#f3e9d2",
+      secondary: "#114b5f",
+      accent: "#D2EFD8",
+      accentHover: "#b4e4be",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
