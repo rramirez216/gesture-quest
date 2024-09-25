c18c44f752ebe9b87df9580eee24f96cc53e8bb7
Ruben Ramirez <rramirez110911@gmail.com>
2024-09-24 21:10:03 -0700

    adds styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <main className='max-h-full h-full relative bg-gray-50'>
+    <main className='max-h-full h-full relative flex flex-col items-center gap-16 justify-center bg-gray-50 p-8 bg-primary'>
       <Home />
     </main>
   )
