a7e5f1704025ed3bbbfc5b30bb994e3295b642e4
Ruben Ramirez <rramirez110911@gmail.com>
2024-09-27 13:25:50 -0700

    removes linaria styles

--- src/components/slider/Image.jsx
+++ src/components/slider/Image.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
-import { styled } from '@linaria/react'
 
 function Image({ src, alt }) {
   return (
     <>
-      <Img src={src} alt={alt} />
+      <img className='w-1/2' src={src} alt={alt} />
     </>
   )
 }
 
-const Img = styled.img`
-  width: 50%;
-`
 export default Image
