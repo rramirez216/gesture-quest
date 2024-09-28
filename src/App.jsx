8abed88145971773b4aa2efb9a8f91ea1a74feb4
Ruben Ramirez <rramirez110911@gmail.com>
2024-09-27 23:59:47 -0700

    d

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <main className='max-h-full h-full relative flex flex-col items-center gap-16 justify-center bg-gray-50 p-8 bg-primary'>
+    <main className='max-h-full h-full relative flex flex-col items-center gap-16 justify-center bg-gray-50 p-8 '>
       <Home />
     </main>
   )
