d7a17fc65fb696b10c98b807a338ea321cadaa1a
Ruben Ramirez <rramirez110911@gmail.com>
2024-09-28 23:53:44 -0700

    changes the primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      primary: "#f3e9d2",
-      secondary: "#114b5f",
+      primary: "#F8F8F7",
+      secondary: "#0E3E4E",
+      secondaryLight: "#2B738B",
       accent: "#D2EFD8",
       accentHover: "#b4e4be",
     },
