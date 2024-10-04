fc622d48ca66d71f99cad6701e7d36b41e3ae0ed
Ruben Ramirez <rramirez110911@gmail.com>
2024-10-04 15:47:10 -0700

    receives new props and adds border and background color to div wrapper

--- src/components/slider/ButtonWrapper.jsx
+++ src/components/slider/ButtonWrapper.jsx
@@ -1,10 +1,15 @@
 import React from "react"
 import Button from "../ui/Button"
 
-function ButtonWrapper({ handleClick, handlePause, timer, handleSliderDisplay, pause }) {
+function ButtonWrapper({ handleClick, handlePause, timer, handleSliderDisplay, pause, imageList, imageIndex }) {
+  const currentImage = imageIndex + 1
+  const totalNumberOfImages = imageList.length
   return (
-    <div className='flex flex-row transform -translate-y-[200%]'>
+    <div className='flex items-center gap-2 text-2xl transform -translate-y-[200%] text-secondary p-2 border border-secondary bg-slate-200'>
       <Button handleButton={handleClick} buttonStr={'Prev'} />
+      <p className='min-w-max'>
+        {`${currentImage} of ${totalNumberOfImages}`}
+      </p>
       <Button handleButton={handleClick} buttonStr={'Next'} />
       <Button
         handleButton={handlePause}
