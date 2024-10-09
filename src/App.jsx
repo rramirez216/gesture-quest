4cfeeedb822c8b8beb05e535c2baf622eebd296d
Ruben Ramirez <rramirez110911@gmail.com>
2024-10-08 21:05:37 -0700

    changes background color

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <main className='max-h-full h-full relative flex flex-col items-center gap-16 justify-center bg-gray-50 px-8'>
+    <main className='max-h-full h-full relative flex flex-col items-center gap-16 justify-center bg-slate-100 px-8'>
       <Home />
     </main>
   )
