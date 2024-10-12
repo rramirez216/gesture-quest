cf402ba9a28e4edb5410a2383e95c80cb9bdad32
Ruben Ramirez <rramirez110911@gmail.com>
2024-10-12 12:15:08 -0700

    adds new button type prop

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Pause, SkipBack, SkipForward, Play, X } from 'react-feather'
 
-function Button({ handleButton, buttonStr }) {
+function Button({ handleButton, buttonStr, buttonType = 'button' }) {
   let icon
 
   switch (buttonStr) {
@@ -26,7 +26,7 @@ function Button({ handleButton, buttonStr }) {
 
   return (
     <>
-      <button className='w-full max-w-xs cursor-pointer px-4 py-2 bg-emerald-400 hover:scale-105 text-2xl text-slate-800' type='button' onClick={() => buttonStr === 'Next' || buttonStr === 'Prev' ? handleButton(buttonStr) : handleButton()}>
+      <button className='w-full max-w-xs cursor-pointer px-4 py-2 bg-emerald-400 hover:scale-105 text-2xl text-slate-800' type={buttonType} onClick={() => buttonStr === 'Next' || buttonStr === 'Prev' ? handleButton(buttonStr) : handleButton()}>
         {icon}
       </button>
     </>
