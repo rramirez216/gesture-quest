2f6667feb6e6acd947bad7fc99e273f3bd4c6fd1
Ruben Ramirez <rramirez110911@gmail.com>
2024-10-12 12:44:30 -0700

    increases font size of file input

--- src/components/ui/FileInput.jsx
+++ src/components/ui/FileInput.jsx
@@ -4,12 +4,13 @@ function FileInput({ handleImage }) {
   return (
     <>
       <input
-        className='w-full max-w-xs bg-emerald-400 hover:scale-105 file:px-4 file:py-2 file:text-2xl file:bg-emerald-400 file:text-slate-800 file:cursor-pointer file:border-0'
+        className='w-full max-w-xs bg-slate-200 border border-slate-400 hover:scale-105 text-xl file:px-4 file:py-2 file:text-2xl file:bg-emerald-400 file:text-slate-800 file:cursor-pointer file:border-0'
         type='file'
         name='file'
         id='file'
         accept='image/*'
         onChange={(event) => handleImage(event)}
+        required
         multiple
       />
     </>
