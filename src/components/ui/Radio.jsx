1eb6618f560bb56c1d84d99dd95e022e41a60ed8
Ruben Ramirez <rramirez110911@gmail.com>
2024-10-14 20:22:01 -0700

    fixes visibility of radio buttons in safari

--- src/components/ui/Radio.jsx
+++ src/components/ui/Radio.jsx
@@ -11,7 +11,7 @@ function Radio({
   return (
     <label className={`block ${checked} px-4 py-2 text-slate-800 cursor-pointer relative select-none has-[:checked]:text-slate-50 hover:scale-105 focus:outline-1 focus:outline-focusOutline`} >
       <input
-        className='mr-2 absolute h-0 w-0 focus:outline-1 focus:outline-focusOutline'
+        className='mr-2 absolute h-0 w-0 invisible'
         type='radio'
         name='interval-times'
         id={labelID}
