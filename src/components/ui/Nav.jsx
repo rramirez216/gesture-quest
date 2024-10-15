e349e876da024df03bced657aadcac36ff8b8784
Ruben Ramirez <rramirez110911@gmail.com>
2024-10-14 20:21:29 -0700

    creates nav component

---
+++ src/components/ui/Nav.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <div className='text-7xl font-leagueSpartan'>
+        Gesture
+      </div>
+      <div className='text-2xl font-quicksand'>Quest</div>
+    </nav>
+  )
+}
+
+export default Nav
