f5245fb29ac05adcc825d2b87357cc71317fddb7
Ruben Ramirez <rramirez110911@gmail.com>
2024-10-21 22:03:36 -0700

    adds green background too logo

--- src/components/ui/Nav.jsx
+++ src/components/ui/Nav.jsx
@@ -3,12 +3,11 @@ import React from "react";
 function Nav() {
   return (
     <nav className='w-full flex justify-center text-slate-800 '>
-      <div className="flex flex-col items-center px-6 py-4">
-        {/*        <div className='w-full h-full px-6 py-4 nav-clip bg-emerald-400'></div> */}
+      <div className='flex flex-col items-center px-6 py-4 relative z-0 before:content-[""] before:absolute before:inset-0 before:-z-10 before:bg-emerald-400'>
         <div className='text-7xl font-leagueSpartan'>
           Gesture
         </div>
-        <div className='text-2xl font-quicksand -mt-4 border-b-2 border-b-emerald-400'>Quest</div>
+        <div className='text-2xl font-quicksand -mt-4'>Quest</div>
       </div>
     </nav>
   )
