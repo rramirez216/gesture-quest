64f29e474d935ac6356e20ecfebe4bf4fa07aa78
Ruben Ramirez <rramirez110911@gmail.com>
2024-10-23 23:28:23 -0700

    changes bg hover of file input

--- src/components/ui/FileInput.jsx
+++ src/components/ui/FileInput.jsx
@@ -5,7 +5,7 @@ function FileInput({ handleImage }) {
   return (
     <div className='relative'>
       <input
-        className='w-full max-w-xs bg-slate-200 border border-slate-400 hover:scale-105 text-xl file:px-4 file:py-2 file:text-2xl file:bg-emerald-400 file:text-slate-800 file:cursor-pointer file:border-0'
+        className='w-full max-w-xs bg-slate-200 border border-slate-400 hover:file:bg-emerald-400 hover:scale-105 text-xl file:px-4 file:py-2 file:text-2xl file:bg-slate-200 file:text-slate-800 file:cursor-pointer file:border-0'
         type='file'
         name='file'
         id='file'
