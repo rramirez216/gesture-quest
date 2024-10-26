8ee0e501d9157cac5163c738faec16a976f5de87
Ruben Ramirez <rramirez110911@gmail.com>
2024-10-25 17:48:18 -0700

    changes border color

--- src/components/slider/ButtonWrapper.jsx
+++ src/components/slider/ButtonWrapper.jsx
@@ -5,7 +5,7 @@ function ButtonWrapper({ handleClick, handlePause, timer, handleSliderDisplay, p
   const currentImage = imageIndex + 1
   const totalNumberOfImages = imageList.length
   return (
-    <div className='max-w-min mx-auto flex gap-2 text-2xl transform -translate-y-[120%] text-slate-800 p-4 border border-slate-300 bg-slate-100'>
+    <div className='max-w-min mx-auto flex gap-2 text-2xl transform -translate-y-[120%] text-slate-800 p-4 border border-slate-400 bg-slate-100'>
       <Button handleButton={handleClick} buttonStr={'Prev'} />
       <p className='min-w-max bg-slate-200 flex flex-col justify-center px-2'>
         <span className="inline-block">{`${currentImage} of ${totalNumberOfImages}`}</span>
