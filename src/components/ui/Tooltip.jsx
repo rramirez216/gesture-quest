8d9f51ad4057f48b30f1bb8a86d83ecfcecd69af
Ruben Ramirez <rramirez110911@gmail.com>
2024-11-03 20:25:00 -0800

    removes tooltip border color

--- src/components/ui/Tooltip.jsx
+++ src/components/ui/Tooltip.jsx
@@ -3,7 +3,7 @@ import { HelpCircle } from 'react-feather'
 
 function Tooltip() {
   return (
-    <div className='absolute -top-7 right-0 group cursor-pointer' aria-describedby='file upload info' tabIndex={'0'}>
+    <div className='absolute -top-7 right-0 group cursor-pointer focus:outline-emerald-900' aria-describedby='file upload info' tabIndex={'0'}>
       <HelpCircle className='group-hover:stroke-amber-500 group-focus:stroke-amber-500' color='#94a3b8' size={24} id='file upload info' role='tooltip' />
       <div className='invisible group-hover:visible absolute group-focus:visible -top-[450%] -right-8 bg-slate-100 w-72 border-2 border-slate-400 p-2 after:absolute after:bottom-2 after:right-7 after:content-[""] after:w-6 after:h-6 after:translate-y-full after:bg-slate-100 after:triangle'>
         <p>
