d1c243eb0df86a0ef88886e946d30ec7e88313a2
Ruben Ramirez <rramirez110911@gmail.com>
2024-11-08 21:34:22 -0800

    adds mobile styles

--- src/components/slider/ButtonWrapper.jsx
+++ src/components/slider/ButtonWrapper.jsx
@@ -2,23 +2,26 @@ import React from "react"
 import Button from "../ui/Button"
 
 function ButtonWrapper({ handleClick, handlePause, timer, handleSliderDisplay, pause, imageList, imageIndex }) {
-  const currentImage = imageIndex + 1
-  const totalNumberOfImages = imageList.length
+  const totalNumberOfImages = `${imageIndex + 1} of ${imageList.length}`
   return (
-    <div className='max-w-min mx-auto flex gap-2 text-2xl transform -translate-y-[120%] text-slate-800 p-4 border border-slate-400 bg-slate-100'>
+    <div className='max-w-min mx-auto flex justify-center gap-1 md:gap-2 text-xl md:text-2xl transform -translate-y-[120%] text-slate-800 border border-slate-300 bg-slate-200 p-2 md:p-4'>
       <Button handleButton={handleClick} buttonStr={'Prev'} />
       <p className='min-w-max bg-slate-200 flex flex-col justify-center px-2'>
-        <span className="inline-block">{`${currentImage} of ${totalNumberOfImages}`}</span>
+        <span className="inline-block">{totalNumberOfImages}</span>
       </p>
       <Button handleButton={handleClick} buttonStr={'Next'} />
-      <Button
-        handleButton={handlePause}
-        buttonStr={pause ? 'Play' : 'Pause'}
-      />
-      <p className='bg-slate-200 flex flex-col justify-center px-2'>
+      <div className='hidden md:block'>
+        <Button
+          handleButton={handlePause}
+          buttonStr={pause ? 'Play' : 'Pause'}
+        />
+      </div>
+      <p className='md:bg-slate-200 md:flex md:flex-col md:justify-center md:px-2 hidden'>
         {timer}
       </p>
-      <Button handleButton={handleSliderDisplay} buttonStr={'End Session'} />
+      <div className="hidden md:block">
+        <Button handleButton={handleSliderDisplay} buttonStr={'End Session'} />
+      </div>
     </div>
 
   )
