a4571637944d829dc7cc5dd06abd2f42cabeab8a
Ruben Ramirez <rramirez110911@gmail.com>
2024-11-13 19:55:26 -0800

    removes timer component and unhides the play and end session button for mobile layout

--- src/components/slider/ButtonWrapper.jsx
+++ src/components/slider/ButtonWrapper.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Button from "../ui/Button"
 
-function ButtonWrapper({ handleClick, handlePause, timer, handleSliderDisplay, pause, imageList, imageIndex }) {
+function ButtonWrapper({ handleClick, handlePause, handleSliderDisplay, pause, imageList, imageIndex }) {
   const totalNumberOfImages = `${imageIndex + 1} of ${imageList.length}`
   return (
     <div className='max-w-min mx-auto flex justify-center gap-1 md:gap-2 text-xl md:text-2xl transform -translate-y-[120%] text-slate-800 border border-slate-300 bg-slate-200 p-2 md:p-4'>
@@ -16,10 +16,7 @@ function ButtonWrapper({ handleClick, handlePause, timer, handleSliderDisplay, p
           buttonStr={pause ? 'Play' : 'Pause'}
         />
       </div>
-      {/*<p className='md:bg-slate-200 md:flex md:flex-col md:justify-center md:px-2 hidden'>
-        {timer}
-      </p>*/}
-      <div className="hidden md:block">
+      <div className="block">
         <Button handleButton={handleSliderDisplay} buttonStr={'End Session'} />
       </div>
     </div>
