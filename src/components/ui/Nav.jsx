265e5f045ef5147d7b31de97ec99440e32934fc7
Ruben Ramirez <rramirez110911@gmail.com>
2024-11-14 22:04:16 -0800

    removes before psuedo class styles and decreases the font size on smaller screens for the title

--- src/components/ui/Nav.jsx
+++ src/components/ui/Nav.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function Nav() {
   return (
     <nav className='w-full flex justify-center text-slate-800 '>
-      <div className='flex flex-col items-center px-6 py-4 relative z-0 before:content-[""] before:absolute before:inset-0 before:-z-10 before:bg-emerald-400'>
-        <div className='text-7xl font-leagueSpartan'>
+      <div className='flex flex-col items-center px-6 py-4 bg-emerald-400 rounded-b-xl'>
+        <div className=' font-leagueSpartan text-6xl md:text-7xl'>
           Gesture
         </div>
-        <div className='text-2xl font-quicksand -mt-4'>Quest</div>
+        <div className='font-quicksand -mt-4 text-2xl'>Quest</div>
       </div>
     </nav>
   )
