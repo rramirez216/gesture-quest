51111786cb9d6e7c31df7c7dc65563fd5f58bfc4
Ruben Ramirez <rramirez110911@gmail.com>
2024-11-14 21:57:54 -0800

    adds gap-8 and adds a media query for gap-16

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Nav from './components/ui/Nav'
 
 function App() {
   return (
-    <main className='max-h-full h-full relative flex flex-col items-center gap-16 justify-center bg-slate-100'>
+    <main className='max-h-full h-full relative flex flex-col items-center gap-8 md:gap-16 justify-center bg-slate-100'>
       <Nav />
       <Home />
     </main>
