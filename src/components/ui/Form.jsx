0f87311d45a1e2a8d826ed2d7a12e007ac2128a7
Ruben Ramirez <rramirez110911@gmail.com>
2024-11-14 22:08:20 -0800

    increases height and reduces gap for smaller screens

--- src/components/ui/Form.jsx
+++ src/components/ui/Form.jsx
@@ -5,10 +5,15 @@ import { constants } from '../../data/constants'
 function Form({ intervalTime, setIntervalTime, children }) {
   let { radioData } = constants
   return (
-    <form className='bg-slate-200 w-full flex flex-col items-center gap-12 max-w-xl py-12' onSubmit={(e) => e.preventDefault()}>
+    <form
+      className='bg-slate-200 w-full h-full md:h-min flex flex-col items-center justify-center gap-8 md:gap-12 max-w-xl py-12'
+      onSubmit={(e) => e.preventDefault()}
+    >
       {children[0]}
       <fieldset className='text-2xl w-full max-w-xs'>
-        <legend className='text-slate-600 pb-2 text-left' >Select Time Per Image</legend>
+        <legend className='text-slate-600 pb-2 text-left'>
+          Select Time Per Image
+        </legend>
         <div className='grid grid-cols-2 gap-2'>
           {radioData.map((item) => (
             <Radio
