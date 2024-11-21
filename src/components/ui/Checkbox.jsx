46c87e90fe450a2f83c210ec43718af7730ed712
Ruben Ramirez <rramirez110911@gmail.com>
2024-11-20 20:21:25 -0800

    creates checkbox component

---
+++ src/components/ui/Checkbox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Checkbox() {
+  return (
+    <>
+      <label className='w-full text-slate-600 text-left'>
+        <span>randomize image order:</span>
+        <input className='' type='checkbox' />
+      </label>
+    </>
+  )
+}
+
+export default Checkbox
