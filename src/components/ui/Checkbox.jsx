08ca61454389b9fc6318505201c63bb3098abc94
Ruben Ramirez <rramirez110911@gmail.com>
2024-11-22 21:41:42 -0800

    adds svg styles

--- src/components/ui/Checkbox.jsx
+++ src/components/ui/Checkbox.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Checkbox() {
+function Checkbox({ randomizeImageList, handleImageOrder }) {
   return (
     <>
-      <label className='w-full text-slate-600 text-center flex justify-center'>
-        <input className='' type='checkbox' />
+      <label className='w-full text-slate-600 text-center flex justify-center' >
+        <input className='' type='checkbox' checked={randomizeImageList} onChange={(event) => handleImageOrder(event)} />
         <svg width={64} height={64} viewBox='-4 -4 39 39' >
-          <rect className='' width={35} height={35} x='-2' y='-2' stroke='#cbd5e1' fill='#cbd5e1' strokeWidth={3} rx='6' ry='6' ></rect>
-          <polyline className='' points='4, 14, 12, 23, 28, 5' stroke='#ffffff' strokeWidth={4} fill='none' ></polyline>
+          <rect className='bg-slate-400' width={35} height={35} x='-2' y='-2' stroke='#cbd5e1' fill='#34d399' strokeWidth={3} rx='6' ry='6' ></rect>
+          <polyline className='' points='4, 14, 12, 23, 28, 5' stroke='#1e293b' strokeWidth={4} fill='none' ></polyline>
         </svg>
         <span>Randomize Image Order</span>
       </label>
