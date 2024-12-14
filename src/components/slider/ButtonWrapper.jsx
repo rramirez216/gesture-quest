da0dcccb531f17101533688c898aba42e81cdacc
Ruben Ramirez <rramirez110911@gmail.com>
2024-12-13 19:55:12 -0800

    adds border radius

--- src/components/slider/ButtonWrapper.jsx
+++ src/components/slider/ButtonWrapper.jsx
@@ -4,7 +4,7 @@ import Button from "../ui/Button"
 function ButtonWrapper({ handleClick, handlePause, handleSliderDisplay, pause, imageList, imageIndex }) {
   const totalNumberOfImages = `${imageIndex + 1} of ${imageList.length}`
   return (
-    <div className='max-w-min mx-auto flex justify-center gap-1 md:gap-2 text-xl md:text-2xl transform -translate-y-[120%] text-slate-800 border border-slate-300 bg-slate-200 p-2 md:p-4'>
+    <div className='max-w-min mx-auto flex justify-center gap-1 md:gap-2 text-xl md:text-2xl transform -translate-y-[100%] text-slate-800 border border-slate-300 rounded-t-lg bg-slate-200 p-2 md:p-4'>
       <Button handleButton={handleClick} buttonStr={'Prev'} />
       <p className='min-w-max bg-slate-200 flex flex-col justify-center px-2'>
         <span className="inline-block">{totalNumberOfImages}</span>
