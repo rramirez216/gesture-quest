6ad6f3ae69f3b0c6708fd1cf47b546a5661c97c7
Ruben Ramirez <rramirez110911@gmail.com>
2024-12-20 20:15:17 -0800

    adds a translateY to the transform function

--- src/components/slider/Image.jsx
+++ src/components/slider/Image.jsx
@@ -4,7 +4,7 @@ function Image({ src, alt, handleMouseDown, handleMouseUp, offset }) {
   const { offsetX, offsetY } = offset
   return (
     <>
-      <img className='h-full object-contain select-none' style={{ transform: `translateX(${offsetX}px)` }} src={src} alt={alt} onMouseDown={(event) => handleMouseDown(event)} onMouseUp={(event) => handleMouseUp(event)} />
+      <img className='h-full object-contain select-none' style={{ transform: `translateX(${offsetX}px) translateY(${offsetY}px)` }} src={src} alt={alt} onMouseDown={(event) => handleMouseDown(event)} onMouseUp={(event) => handleMouseUp(event)} />
     </>
   )
 }
