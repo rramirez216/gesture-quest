ac01a57a227813fbe6153a910a4509d077ab96c3
Ruben Ramirez <rramirez110911@gmail.com>
2024-12-21 18:22:09 -0800

    passes imageSize props and adds scale() function to in-line style attribute

--- src/components/slider/Image.jsx
+++ src/components/slider/Image.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function Image({ src, alt, handleMouseDown, handleMouseUp, offset }) {
+function Image({ src, alt, handleMouseDown, handleMouseUp, offset, imageSize }) {
   const { offsetX, offsetY } = offset
   return (
     <>
-      <img className='h-full object-contain select-none' style={{ transform: `translateX(${offsetX}px) translateY(${offsetY}px)` }} src={src} alt={alt} onMouseDown={(event) => handleMouseDown(event)} onMouseUp={(event) => handleMouseUp(event)} />
+      <img className='h-full object-contain select-none' style={{ transform: `translate(${offsetX}px, ${offsetY}px) scale(${imageSize})` }} src={src} alt={alt} onMouseDown={(event) => handleMouseDown(event)} onMouseUp={(event) => handleMouseUp(event)} />
     </>
   )
 }
