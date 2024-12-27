a61e190707b72ab3e0a337fc0296fe0841406b7b
Ruben Ramirez <rramirez110911@gmail.com>
2024-12-26 18:45:25 -0800

    removes comments

--- src/hooks/useMouseDownUp.js
+++ src/hooks/useMouseDownUp.js
@@ -6,23 +6,12 @@ function useMouseDownUp(movement = { x: 0, y: 0 }) {
 
   React.useEffect(() => {
     if (isMouseDown === true) {
-      //if ((movement.x > 0 && move) || movement.x < 0) {
       let { offsetX, offsetY } = {
         offsetX: offset.offsetX + movement.x,
         offsetY: offset.offsetY + movement.y,
       };
       setOffSet({ offsetX, offsetY });
       console.log("X:", offsetX, "Y:", offsetY);
-      //}
-
-      /*      if (movement.y > 0 || movement.y < 0) {
-        let { offsetX, offsetY } = {
-          offsetX: offset.offsetX,
-          offsetY: offset.offsetY + movement.y,
-        };
-        setOffSet({ offsetX, offsetY });
-        //console.log("Y:", offsetY);
-      }*/
     }
   }, [isMouseDown, movement]);
 
