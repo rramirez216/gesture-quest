e669b02540b98bd29a87f350ba3867a0a90ae8c4
Ruben Ramirez <rramirez110911@gmail.com>
2024-12-27 16:28:47 -0800

    adds cursor style property with a value of move

--- src/components/slider/Image.jsx
+++ src/components/slider/Image.jsx
@@ -4,7 +4,7 @@ function Image({ src, alt, handleMouseDown, handleMouseUp, offset, imageSize }) 
   const { offsetX, offsetY } = offset
   return (
     <>
-      <img className='h-full object-contain select-none' style={{ transform: `translate(${offsetX}px, ${offsetY}px) scale(${imageSize})` }} src={src} alt={alt} onMouseDown={(event) => handleMouseDown(event)} onMouseUp={(event) => handleMouseUp(event)} />
+      <img className='h-full object-contain select-none cursor-move' style={{ transform: `translate(${offsetX}px, ${offsetY}px) scale(${imageSize})` }} src={src} alt={alt} onMouseDown={(event) => handleMouseDown(event)} onMouseUp={(event) => handleMouseUp(event)} />
     </>
   )
 }
