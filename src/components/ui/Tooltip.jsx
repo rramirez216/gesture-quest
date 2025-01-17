2c2bb470b508409e7ef8881b21d86c62e69570d9
Ruben Ramirez <rramirez110911@gmail.com>
2025-01-16 19:34:33 -0800

    changes the top and right css properties and adds translate Y

--- src/components/ui/Tooltip.jsx
+++ src/components/ui/Tooltip.jsx
@@ -3,11 +3,11 @@ import { HelpCircle } from 'react-feather'
 
 function Tooltip() {
   return (
-    <div className='w-min -top-7 right-0 group cursor-pointer focus:outline-emerald-900' aria-describedby='file upload info' tabIndex={'0'}>
+    <div className='absolute top-0 right-0 group cursor-pointer transform -translate-y-full focus:outline-emerald-700' aria-describedby='file upload info' tabIndex={'0'}>
       <HelpCircle className='group-hover:stroke-amber-500 group-focus:stroke-amber-500' color='#94a3b8' size={24} id='file upload info' role='tooltip' />
-      <div className='invisible group-hover:visible absolute group-focus:visible top-[10px] -right-8 bg-slate-100 w-72 border-2 border-slate-400 p-2 after:absolute after:bottom-2 after:right-7 after:content-[""] after:w-6 after:h-6 after:translate-y-full after:bg-slate-100 after:triangle'>
+      <div className='invisible group-hover:visible absolute group-focus:visible top-0 right-0 transform -translate-y-full translate-x-full bg-slate-100 w-72 border-2 border-slate-400 p-2 rounded-lg'>
         <p>
-          The images selected stay local to the user's machine and are not uploaded to a server.
+          The images selected stay local to the user's machine and do not get uploaded to a server.
         </p>
       </div>
     </div>
