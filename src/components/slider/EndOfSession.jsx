30989d545bad067f023eb6f8e371b9e43111ed15
Ruben Ramirez <rramirez110911@gmail.com>
2025-01-26 20:56:29 -0800

    adds background color and centers buttons

--- src/components/slider/EndOfSession.jsx
+++ src/components/slider/EndOfSession.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Button from '../ui/Button'
 
 function EndOfSession({ handleSliderDisplay, updateImageIndex }) {
-  return <div className='absolute'>
+  return <div className='absolute bg-slate-100 inset-0 flex items-center justify-center'>
     <Button handleButton={updateImageIndex} buttonStr={'repeat'} />
     <Button handleButton={handleSliderDisplay} buttonStr={'Create New Session'} />
   </div>
