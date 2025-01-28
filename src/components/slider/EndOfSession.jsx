bf878d323ea9043d6c6431a2508244ac0be404c3
Ruben Ramirez <rramirez110911@gmail.com>
2025-01-27 20:18:12 -0800

    adds ternary to determine styles of the compoent

--- src/components/slider/EndOfSession.jsx
+++ src/components/slider/EndOfSession.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Button from '../ui/Button'
 
-function EndOfSession({ handleSliderDisplay, updateImageIndex }) {
-  return <div className='absolute bg-slate-100 inset-0 flex items-center justify-center'>
+function EndOfSession({ handleSliderDisplay, updateImageIndex, sessionEnd }) {
+  const sessionEndStyles = sessionEnd ? 'absolute bg-slate-100 inset-0 flex items-center justify-center' : 'absolute bg-slate-100 inset-0 hidden items-center justify-center'
+  return <div className={sessionEndStyles}>
     <Button handleButton={updateImageIndex} buttonStr={'repeat'} />
     <Button handleButton={handleSliderDisplay} buttonStr={'Create New Session'} />
   </div>
