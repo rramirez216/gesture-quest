1cd195ed93dd03b9109eea0e3550bca8038ee726
Ruben Ramirez <rramirez110911@gmail.com>
2025-02-03 10:49:09 -0800

    passes sessionEnd as props into the buttonWrapper compenent

--- src/components/slider/Modal.jsx
+++ src/components/slider/Modal.jsx
@@ -90,6 +90,7 @@ function Modal({
           pause={pause}
           imageList={imageList}
           imageIndex={imageIndex}
+          sessionEnd={sessionEnd}
         />
       </div>
       <p className='w-24 text-center text-slate-800 bg-emerald-400 absolute top-0 text-2xl rounded-b-lg'>
