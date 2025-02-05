156f6eb03c0179f5f2fab4adf736acb66d0a63d2
Ruben Ramirez <rramirez110911@gmail.com>
2025-02-04 21:15:42 -0800

    creates footer component

---
+++ src/components/ui/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+function Footer() {
+  return (
+    <footer className='text-slate-800'>&copy; 2024 Ruben Ramirez. All Rights Reserved.</footer>
+  )
+}
+
+export default Footer
