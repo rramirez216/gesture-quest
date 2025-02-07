7a3c749acc7378b2237ff56c2b05e7bcab80c746
Ruben Ramirez <rramirez110911@gmail.com>
2025-02-07 12:25:59 -0800

    changes wrapper tag from div to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Home from './pages/Home'
 import Nav from './components/ui/Nav'
+import Footer from './components/ui/Footer'
 
 function App() {
   return (
-    <main className='max-h-full h-full relative flex flex-col items-center gap-8 md:gap-16 justify-center bg-slate-100'>
+    <div className='max-h-full h-full relative flex flex-col items-center gap-8 md:gap-16 justify-center bg-slate-100'>
       <Nav />
       <Home />
-    </main>
+      <Footer />
+    </div>
   )
 }
 
