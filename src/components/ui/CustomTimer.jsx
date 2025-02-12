c81c4f61e7cc97c4650a77ee1018c382993a5f8d
Ruben Ramirez <rramirez110911@gmail.com>
2025-02-11 22:09:16 -0800

    adds react funtional component structure

---
+++ src/components/ui/CustomTimer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from './Button'
+
+function CustomTimer() {
+  return (<div>
+    <Button buttonStr={'Custom'} />
+  </div>)
+}
+
+export default CustomTimer
