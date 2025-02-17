aa1c0caf409fdd233b87ee2185071c58e5baccd9
Ruben Ramirez <rramirez110911@gmail.com>
2025-02-16 22:10:57 -0800

    adds number input

--- src/components/custom-timer-button/CustomTimer.jsx
+++ src/components/custom-timer-button/CustomTimer.jsx
@@ -4,7 +4,9 @@ import Button from '../ui/Button'
 function CustomTimer({ intervalTime: { radioNum }, handleCustomTimeButton }) {
   return (<div className='flex'>
     <Button buttonStr={'-'} handleButton={handleCustomTimeButton} />
-    <p className=''>{radioNum}</p>
+    <input type='number' min={0} max={60} />
+    <p className='text-2xl'>:</p>
+    <input type='number' min={0} max={59} />
     <Button buttonStr={'+'} handleButton={handleCustomTimeButton} />
   </div>)
 }
