2d4a88bb4123b843f90aed41fc5e6b0e1e6c1759
Ruben Ramirez <rramirez110911@gmail.com>
2025-02-21 19:11:40 -0800

    creates handleOnChange function and a handleCustomTimerFunction

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Form from '../components/ui/Form'
 import Button from '../components/ui/Button'
 import Modal from '../components/slider/Modal'
 import CustomTimer from '../components/custom-timer-button/CustomTimer'
-import shuffleArray from './Home.helpers.js'
+import { shuffleArray, roundToNearestTen } from './Home.helpers.js'
 
 function Home() {
   const [imageList, setImageList] = React.useState([])
@@ -13,6 +13,7 @@ function Home() {
     radioStr: 'secs',
   })
   const [sliderDisplay, setSliderDisplay] = React.useState(false)
+  const [customTime, setCustomTime] = React.useState({ minutes: 0, seconds: 0 })
 
   const handleImage = (event) => {
     let arr = []
@@ -34,18 +35,69 @@ function Home() {
   }
 
   const handleCustomTimeButton = (str) => {
-    if (str === '+') {
-      setIntervalTime((prevIntervalTime) => ({
-        ...prevIntervalTime,
-        radioNum: prevIntervalTime.radioNum + 5,
-      }))
-    } else {
-      setIntervalTime((prevIntervalTime) => ({
-        ...prevIntervalTime,
-        radioNum: prevIntervalTime.radioNum - 5,
-      }))
+    const { minutes, seconds } = customTime
+    if (str === '+sec' && (seconds == 59 || seconds >= 55)) {
+      setCustomTime((prev) => ({ ...prev, seconds: 59 }))
+      return
+    }
+    if (str === '+sec' && seconds % 5 !== 0) {
+      setCustomTime((prev) => ({ ...prev, seconds: roundToNearestTen(str, seconds) }))
+      return
+    }
+    if (str === '+sec') {
+      setCustomTime((prev) => ({ ...prev, seconds: prev.seconds + 5 }))
+      return
+    }
+    if (str === '-sec' && seconds > 55) {
+      setCustomTime((prev) => ({ ...prev, seconds: 55 }))
+      return
+    }
+    if (str === '-sec' && seconds - 5 < 0) {
+      setCustomTime((prev) => ({ ...prev, seconds: 0 }))
+      return
+    }
+    if (str === '-sec' && seconds % 5 !== 0) {
+      setCustomTime((prev) => ({ ...prev, seconds: roundToNearestTen(str, seconds) }))
+      return
+    }
+    if (str === '-sec') {
+      setCustomTime((prev) => ({ ...prev, seconds: prev.seconds - 5 }))
+      return
+    }
+    if (str === '+min' && (minutes == 59)) {
+      setCustomTime((prev) => ({ ...prev, minutes: 59 }))
+      return
+    }
+    if (str === '+min') {
+      setCustomTime((prev) => ({ ...prev, minutes: prev.minutes + 1 }))
+      return
+    }
+    if (str === '-min' && minutes == 0) {
+      setCustomTime((prev) => ({ ...prev, minutes: 0 }))
+      return
+    }
+    if (str === '-min') {
+      setCustomTime((prev) => ({ ...prev, minutes: prev.minutes - 1 }))
+      return
     }
   }
+
+  const handleOnChange = (event) => {
+    let strToNum = Number(event.target.value)
+
+    if (event.target.value < 0) {
+      setCustomTime((prev) => ({ ...prev, seconds: 0 }))
+      return
+    }
+
+    if (event.target.value > 59) {
+      setCustomTime((prev) => ({ ...prev, seconds: 59 }))
+      return
+    }
+
+    setCustomTime((prev) => ({ ...prev, seconds: strToNum }))
+  }
+
 
   React.useEffect(() => {
     return () => {
@@ -60,7 +112,8 @@ function Home() {
     <main className='w-full h-full flex flex-col items-center justify-center md:px-8'>
       <Form intervalTime={intervalTime} setIntervalTime={setIntervalTime} sliderDisplay={sliderDisplay}>
         <FileInput handleImage={handleImage} />
-        <CustomTimer intervalTime={intervalTime} handleCustomTimeButton={handleCustomTimeButton} />
+        <CustomTimer time={customTime.minutes} handleOnChange={handleOnChange} setCustomTime={setCustomTime} handleCustomTimeButton={handleCustomTimeButton} minus={'-min'} plus={'+min'} />
+        <CustomTimer time={customTime.seconds} handleOnChange={handleOnChange} setCustomTime={setCustomTime} handleCustomTimeButton={handleCustomTimeButton} minus={'-sec'} plus={'+sec'} />
         <Button handleButton={handleSliderDisplay} buttonStr={'Start'} buttonType={'submit'} />
       </Form>
       {sliderDisplay && (
