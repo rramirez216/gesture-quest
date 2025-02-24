3e3831fb28ba828934e926d0048eb81067daadbe
Ruben Ramirez <rramirez110911@gmail.com>
2025-02-23 21:24:08 -0800

    adds label tag

--- src/components/custom-timer-button/CustomTimer.jsx
+++ src/components/custom-timer-button/CustomTimer.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Button from '../ui/Button'
 
-function CustomTimer({ time, handleOnChange, setCustomTime, handleCustomTimeButton, minus, plus }) {
-  return (<div className='flex'>
-    <Button buttonStr={minus} handleButton={handleCustomTimeButton} />
-    <input type='number' min={0} max={59} value={time} onChange={handleOnChange} />
-    <Button buttonStr={plus} handleButton={handleCustomTimeButton} />
+function CustomTimer({ time, handleOnChange, handleCustomTimeButton, minus, plus }) {
+  return (<div className='flex flex-col'>
+    <label htmlFor='number-input'>Min</label>
+    <div className='flex'>
+      <Button buttonStr={minus} handleButton={handleCustomTimeButton} />
+      <input type='number' min={0} max={59} value={time} onChange={handleOnChange} id='number-input' />
+      <Button buttonStr={plus} handleButton={handleCustomTimeButton} />
+    </div>
   </div>)
 }
 
