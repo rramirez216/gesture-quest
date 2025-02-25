3096275558eef85850effc888436192c2fdb929f
Ruben Ramirez <rramirez110911@gmail.com>
2025-02-25 10:34:30 -0800

    restructures the layout of the customtimer component by adding label tag

--- src/components/custom-timer-button/CustomTimer.jsx
+++ src/components/custom-timer-button/CustomTimer.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Button from '../ui/Button'
 
-function CustomTimer({ time, handleOnChange, handleCustomTimeButton, minus, plus }) {
-  return (<div className='flex flex-col'>
-    <label htmlFor='number-input'>Min</label>
-    <div className='flex'>
-      <Button buttonStr={minus} handleButton={handleCustomTimeButton} />
-      <input type='number' min={0} max={59} value={time} onChange={handleOnChange} id='number-input' />
-      <Button buttonStr={plus} handleButton={handleCustomTimeButton} />
+function CustomTimer({ time, handleOnChange, handleCustomTimeButton, minus, plus, labelText }) {
+  return (
+    <div className='flex flex-col w-full gap-2'>
+      <label className='text-slate-600' htmlFor="number-input">{labelText}</label>
+      <div className='flex w-full'>
+        <Button buttonStr={minus} handleButton={handleCustomTimeButton} />
+        <input className='w-full h-full text-center' type='number' min={0} max={59} value={time} onChange={handleOnChange} id='number-input' />
+        <Button buttonStr={plus} handleButton={handleCustomTimeButton} />
+      </div>
     </div>
-  </div>)
+  )
 }
 
 export default CustomTimer
