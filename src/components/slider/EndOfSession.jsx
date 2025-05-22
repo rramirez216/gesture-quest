f619c5d3523066f3c31988f6baeb2e83140b74b4
Ruben Ramirez <rramirez110911@gmail.com>
2025-05-21 21:56:15 -0700

    updates stlyes to add a flex column layout for the end of session ui

--- src/components/slider/EndOfSession.jsx
+++ src/components/slider/EndOfSession.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Button from '../ui/Button'
 
 function EndOfSession({ handleSliderDisplay, updateImageIndex, sessionEnd }) {
-  const sessionEndStyles = sessionEnd ? 'absolute bg-slate-100 inset-0 flex items-center justify-center gap-4' : 'hidden'
+  const sessionEndStyles = sessionEnd ? 'absolute bg-slate-100 inset-0 flex flex-col items-center justify-center gap-4' : 'hidden'
   return <div className={sessionEndStyles}>
-    <p>All Done!</p>
-    <Button handleButton={updateImageIndex} buttonStr={'Repeat'} buttonType='submit' />
-    <Button handleButton={handleSliderDisplay} buttonStr={'Create New Session'} />
+    <div>
+      <p className='text-5xl'>All Done!</p>
+    </div>
+    <div className='flex'>
+      <Button handleButton={updateImageIndex} buttonStr={'Repeat'} buttonType='submit' />
+      <Button handleButton={handleSliderDisplay} buttonStr={'Create New Session'} />
+    </div>
   </div>
 }
 
